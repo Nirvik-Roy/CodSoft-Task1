d2d62949a4c81537803ae297f7866d416fc7fd6b
Nirvik Roy <nirvikroy802@gmail.com>
2024-06-22 15:43:27 +0530

    fourth commit

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,176 @@
+import React, { useEffect, useRef } from 'react'
+import './Home.scss'
+import Mylogo from '../../assets/_DSC9933 (2).jpg'
+import { useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import About from '../About/About'
+import Skills from '../Skills/Skills'
+gsap.registerPlugin(ScrollTrigger);
+
+const Home = () => {
+ 
+
+  const mousemove = (e) =>{
+   
+    gsap.to('.cursor',{
+      x:e.pageX,
+      y:e.pageY,
+      ease:'back.out'
+    })
+  }
+useGSAP(()=>{
+    let tl=gsap.timeline()
+    tl.from('.nav_img_div',{
+      duration:0.3,
+      opacity:0,
+      y:-30,
+    })
+    tl.from(".nav_logo",{
+      duration:0.3,
+      opacity:0,
+      y:-30,
+    })
+   tl.from('.nav_links',{
+    y:-30,
+    duration:0.3,
+    opacity:0,
+    stagger:0.15,
+   })
+   tl.from('.intro_para',{
+    x:-300,
+    duration:0.3,
+    opacity:0,
+   })
+   tl.from(".intro_heading",{
+    x:-300,
+    duration:0.3,
+    opacity:0,
+    
+   })
+  
+   tl.from(".introduction_para",{
+    x:-300,
+    duration:0.3,
+    opacity:0,
+   })
+   tl.from(".intro_btn",{
+    opacity:0,
+   })
+   tl.from('.hero_section_right_img_div',{
+    duration:0.3,
+    scale:6,
+    opacity:0,
+   },'-=1')
+   tl.from(".social_para",{
+    duration:0.2,
+    opacity:0,
+    y:30,
+    stagger:0.15,
+   })
+   tl.from('.social_btn',{
+    duration:0.3,
+    opacity:0,
+    y:30,
+    stagger:0.15,
+   })
+   tl.from('.intro_heading2',{
+    opacity:0,
+    duration:0.3
+   })
+})
+  return (
+    <>
+      <div onMouseMove={((e)=>mousemove(e))} className='container_main'>
+        <div className='cursor'></div>
+       <nav className='nav_main'>
+      <div className='nav_bar_main d-flex justify-content-between align-items-center'>
+    <div className='nav_logo_main '>
+      <div className='nav_img_div_main d-flex'>
+        <div className='nav_img_div mt-2'>
+          <img className='nav_logo_img' src={Mylogo} alt='logo' />
+        </div>
+      </div>
+      <div className='nav_logo_div'>
+        <p className=' nav_logo mt-3'>NEO</p>
+      </div>
+    </div>
+    <div className='nav_div_wrapper'>
+      <ul className='text-light nav_links_main'>
+        <li className='nav_links'>HOME</li>
+        <li className='nav_links'>ABOUT</li>
+        <li className='nav_links'>SKILLS</li>
+        <li className='nav_links'>PORTFOLIO</li>
+        <li className='nav_links'>CONTACT</li>
+        <li className='nav_links download_CV btn'>
+          <a href='../public/My Resume (4)-1.pdf' download className='download_para'>Download CV</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+ 
+  <div className='hero_section_main d-flex justify-content-center'>
+  <div className='hero_section d-flex justify-content-space-around'>
+    <div className='hero_section_left'>
+      <div className='hero_section_left_text'>
+      <p className='intro_para'>Welcome To My World</p>
+        <h1 className='text-light intro_heading'>Hi, I'm <span className='my_name'>Nirvik Roy</span></h1>
+        <p className='d-inline text-light intro_heading'>a </p> <p className='intro_heading2 text-light d-inline'>Front-End Developer</p>
+        
+       
+         
+        
+       
+        <div className='introduction_div'>
+          <p className='introduction_para'>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways tha</p>
+        </div>
+      </div>
+      <div className='social_skills_main'>
+
+      <div className='social_skills_wrapper'>
+        <p className='social_para'>Find With Me</p>
+        <div className='d-flex justify-content-between'>
+          <div className='social_media_buttons'>
+            <a className='social_btn'><i class="fa-brands fa-facebook-f text-light"></i></a>
+            <a className='social_btn '><i class="fa-brands fa-instagram"></i></a>
+            <a className='social_btn'><i class="fa-brands fa-linkedin"></i></a>
+          </div>
+
+        </div>
+</div>
+       <div className='social_skills_wrapper'>
+        <p className='social_para'>BEST SKILLS ON</p>
+        <div className='d-flex justify-content-between'>
+          <div className='social_media_buttons'>
+            <button className='social_btn btn'><i class="fa-brands fa-html5 "></i></button>
+            <button className='social_btn btn'><i class="fa-brands fa-react text-primary"></i></button>
+            <button className='social_btn btn'><i class="fa-brands fa-js text-warning"></i></button>
+          </div>
+
+        </div>
+</div>
+      </div>
+    </div>
+
+    <div className='hero_section_right'>
+      <div className='hero_section_right_img_div_main'>
+        <div className='hero_section_right_img_div'>
+          <img className='hero_section_right_img' src={Mylogo}></img>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div className='d-flex justify-content-center mt-5'>
+    <hr className='hr_line'/>
+  </div>
+      </div>
+
+      <About/>
+      <Skills/>
+    </>
+  )
+}
+
+export default Home
---
+++ src/Component/Skills/Skills.jsx
@@ -0,0 +1,281 @@
+
+import React from 'react'
+import './Skills.scss'
+import { useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Timeline } from 'gsap/gsap-core';
+gsap.registerPlugin(ScrollTrigger);
+const Skills = () => {
+
+    const mousemove = (e) =>{
+   
+        gsap.to('.cursor',{
+          x:e.pageX,
+          y:e.pageY,
+          ease:'back.out'
+        })
+      }
+    useGSAP(()=>{
+       
+        gsap.to('.percentage',{
+            width:"100%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage1',{
+            width:"80%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage2',{
+            width:"75%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage3',{
+            width:"70%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage4',{
+            width:"90%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage5',{
+            width:"60%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage6',{
+            width:"80%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage7',{
+            width:"65%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage8',{
+            width:"70%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+
+        gsap.to('.percentage9',{
+            width:"90%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage10',{
+            width:"60%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+        gsap.to('.percentage11',{
+            width:"50%",
+            duration:0.6,
+            scrollTrigger:{
+                trigger:'.percentage',
+                scroller:"body",
+                start:'top 100%',
+            }
+        })
+    })
+  return (
+    <>
+      <section className='skills_section' onMouseMove={((e)=>mousemove(e))}>
+      <div className='cursor'></div>
+      <h1 className='skills_heading text-center'>My Skills</h1>
+        <div className='skills_container_main d-flex justify-content-center'>
+            <div className='skills_main_div d-flex'>
+                 <div className='left_skills_div'>
+                 <div className='left_skills_main'>
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>HTML</p>
+                        <p className='skill_percentage'>100%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>CSS</p>
+                        <p className='skill_percentage'>80%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage1'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>JavaScript</p>
+                        <p className='skill_percentage'>75%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage2'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>React JS</p>
+                        <p className='skill_percentage'>70%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage3'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>BootStrap</p>
+                        <p className='skill_percentage'>90%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage4'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>Angular Js</p>
+                        <p className='skill_percentage'>60%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage10'></div>
+                        </div>
+                    </div>
+
+                    </div>
+                 </div>
+
+
+
+
+                 
+                 <div className='right_skills_div'>
+                 <div className='right_skills_main'>
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>Material UI</p>
+                        <p className='skill_percentage'>60%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage5'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>SCSS</p>
+                        <p className='skill_percentage'>80%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage6'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>GSAP</p>
+                        <p className='skill_percentage'>65%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage7'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>Redux Toolkit</p>
+                        <p className='skill_percentage'>70%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage8'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>Git/GitHub</p>
+                        <p className='skill_percentage'>90%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage9'></div>
+                        </div>
+                    </div>
+
+                    <div>
+                    <div className='d-flex justify-content-between skill_head_div'>
+                        <p className='skill_1'>TypeScript</p>
+                        <p className='skill_percentage'>50%</p>
+                        </div>
+                        <div className='percentage_div'>
+                            <div className='percentage11'></div>
+                        </div>
+                    </div>
+
+                    </div>
+                 </div>
+            </div>
+        </div>
+        <div className='d-flex justify-content-center mt-4'>
+    <hr className='hr_line'/>
+  </div>
+      </section>
+    </>
+  )
+}
+
+export default Skills
